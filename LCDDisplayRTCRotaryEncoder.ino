514af069599de9016418e4884cd0bb102a475d75
Boban <56995138+BobanNikolov@users.noreply.github.com>
2021-02-14 17:52:31 +0100

    Update LCDDisplayRTCRotaryEncoder.ino

--- LCDDisplayRTCRotaryEncoder.ino
+++ LCDDisplayRTCRotaryEncoder.ino
@@ -1,189 +1,222 @@
-//3
-#include <DS3232RTC.h>
-#include <TimeLib.h>
-#include <Streaming.h>
-#include <Wire.h> 
+#include <Wire.h>
+#include <DS3231.h>
 #include <LiquidCrystal_I2C.h>
+#include "PushButton.h"
+DS3231 RTC;
+LiquidCrystal_I2C lcd(0x27,16,2);
 #define CLK 2
 #define DT 3
 #define SW 4
-const unsigned long KEY_HOLD=100;
-unsigned long buttonPressTime;
-bool buttonDown=false;
-enum clockState { UNDEFINED, RUNNING, SETSEC, SETMIN, SETHOUR } setupState;
-unsigned long int lastSetupTime=0;
+#define buttonUP 2
+#define buttonDOWN 1
+#define buttonNOCHANGE 3
+boolean setupDone=false;
+enum clockState { UNDEFINED=0, RUNNING, SETDAY, SETMON, SETYEAR, SETDOW, SETHOUR, SETMIN, SETSEC } setupState;
+PushButton buttonSETUP(SW);
+bool help=false;
+bool century = false;
+bool h12Flag=false;
+bool pmFlag=false;
+String DoW[7]={"MON","TUE","WED","THU","FRI","SAT","SUN"};
 int counter=0;
 int currentStateCLK;
 int lastStateCLK;
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-void setup(){
-  lcd.init();                      // initialize the lcd 
-  lcd.backlight();
-  Serial.begin(115200);
-  pinMode(CLK,INPUT);
-  pinMode(DT,INPUT);
-  pinMode(SW, INPUT_PULLUP);
-  lastStateCLK=digitalRead(CLK);
-  lcd.setCursor(0,0);
-  lcd.print("WELCOME");
-  setupState=RUNNING;
-  setSyncProvider(RTC.get);
-  Serial << F("RTC Sync");
-  if (timeStatus() != timeSet) Serial << F(" FAIL!");
-}
-void loop()
-{
-    static time_t tLast;
-    time_t t;
-    tmElements_t tm;
-
-    // check for input to set the RTC, minimum length is 12, i.e. yy,m,d,h,m,s
-    if (Serial.available() >= 12) {
-        // note that the tmElements_t Year member is an offset from 1970,
-        // but the RTC wants the last two digits of the calendar year.
-        // use the convenience macros from the Time Library to do the conversions.
-        int y = Serial.parseInt();
-        if (y >= 100 && y < 1000)
-            Serial << F("Error: Year must be two digits or four digits!") << endl;
-        else {
-            if (y >= 1000)
-                tm.Year = CalendarYrToTm(y);
-            else    // (y < 100)
-                tm.Year = y2kYearToTm(y);
-            tm.Month = Serial.parseInt();
-            tm.Day = Serial.parseInt();
-            tm.Hour = Serial.parseInt();
-            tm.Minute = Serial.parseInt();
-            tm.Second = Serial.parseInt();
-            t = makeTime(tm);
-            RTC.set(t);        // use the time_t value to ensure correct weekday is set
-            setTime(t);
-            printDateTime(t);
-            // dump any extraneous input
-            while (Serial.available() > 0) Serial.read();
-        }
-    }
-
-    t = now();
-    if (t != tLast) {
-      if(setupState==RUNNING){
-        tLast = t;
-        printDateTime(t);
-      }
-        if (second(t) == 0) {
-            float c = RTC.temperature() / 4.;
-            float f = c * 9. / 5. + 32.;
-            if(setupState==RUNNING){
-              printDateTimeTemperature(t,c,f);
-            }
-        }
-    }
-    if(digitalRead(SW)==LOW){
-      delay(100);
-      if(digitalRead(SW)==LOW){
-      if(millis()-lastSetupTime>50){
-        Serial.println("Button pressed!");
-        lastSetupTime=millis();
+byte buttonStateSETUP=buttonNOCHANGE;
+void checkSwitchCondition(void){
+  buttonStateSETUP=buttonSETUP.stateChanged();
+  if(buttonStateSETUP==buttonUP){
+    Serial.println("Button pressed!");
+    Serial.println(setupState);
+  if(setupState==RUNNING){
         Serial.println(setupState);
-        counter=0;
-      if(setupState==RUNNING){
+        setupState=SETDAY;
+        lcd.setCursor(1,0);
+        lcd.blink();
+      }else if(setupState==SETDAY){
+        Serial.println(setupState);
+        setupState=SETMON;
+        lcd.setCursor(1,0);
+        lcd.noBlink();
+        lcd.setCursor(4,0);
+        lcd.blink();
+      }else if(setupState==SETMON){
+        Serial.println(setupState);
+        setupState=SETYEAR;
+        lcd.setCursor(5,0);
+        lcd.noBlink();
+        lcd.setCursor(9,0);
+        lcd.blink();
+      }else if(setupState==SETYEAR){
+        Serial.println(setupState);
+        setupState=SETDOW;
+        lcd.setCursor(9,0);
+        lcd.noBlink();
+        lcd.setCursor(13,0);
+        lcd.blink();
+      }else if(setupState==SETDOW){
         Serial.println(setupState);
         setupState=SETHOUR;
-        lcd.setCursor(0,1);
+        lcd.setCursor(13,0);
+        lcd.noBlink();
+        lcd.setCursor(1,1);
         lcd.blink();
       }else if(setupState==SETHOUR){
         Serial.println(setupState);
-        lcd.setCursor(0,1);
+        lcd.setCursor(1,1);
         lcd.noBlink();
-        setupState=SETMIN;
-        lcd.setCursor(3,1);
+        lcd.setCursor(4,1);
         lcd.blink();
+        setupState=SETMIN;
       }else if(setupState==SETMIN){
         Serial.println(setupState);
-        lcd.setCursor(3,1);
+        lcd.setCursor(4,1);
         lcd.noBlink();
-        setupState=SETSEC;
         lcd.setCursor(7,1);
         lcd.blink();
+        setupState=SETSEC;
       }else if(setupState==SETSEC){
         Serial.println(setupState);
-        lcd.setCursor(7,1);
+       lcd.setCursor(7,1);
         lcd.noBlink();
         setupState=RUNNING;
       }
-      }
-     }
-   }
-   currentStateCLK = digitalRead(CLK);
-   if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
-    lastSetupTime=millis();
+  }
+}
+void printToLCD(){
+  lcd.setCursor(0,0);
+  lcd.print(RTC.getDate(),DEC);
+  lcd.setCursor(2,0);
+  lcd.print("/");
+  lcd.setCursor(3,0);
+  lcd.print(RTC.getMonth(century),DEC);
+  lcd.setCursor(5,0);
+  lcd.print("/");
+  lcd.setCursor(6,0);
+  lcd.print("20");
+  lcd.setCursor(8,0);
+  lcd.print(RTC.getYear(),DEC);
+  lcd.setCursor(11,0);
+  lcd.print(DoW[RTC.getDoW()-1]);
+  if(RTC.getHour(h12Flag,pmFlag)<10){
+    lcd.setCursor(1,1);
+  }else{
+    lcd.setCursor(0,1);
+  }
+  lcd.print(RTC.getHour(h12Flag,pmFlag),DEC);
+  lcd.setCursor(2,1);
+  lcd.print(":");
+  if(RTC.getMinute()<10){
+    lcd.setCursor(4,1);
+  }else{
+    lcd.setCursor(3,1);
+  }
+  lcd.print(RTC.getMinute(),DEC);
+  lcd.setCursor(5,1);
+  lcd.print(":");
+  lcd.setCursor(6,1);
+  lcd.print(RTC.getSecond(),DEC);
+  lcd.setCursor(11,1);
+  lcd.print(RTC.getTemperature(),1);
+  lcd.setCursor(15,1);
+  lcd.print((char)223);
+}
+void setup() {
+  // put your setup code here, to run once:
+  lcd.init();
+  lcd.backlight();
+  Wire.begin();
+  Serial.begin(9600);
+  pinMode(CLK,INPUT);
+  pinMode(DT,INPUT);
+  pinMode(SW, INPUT_PULLUP);
+  lastStateCLK = digitalRead(CLK);
+  setupState=RUNNING;
+  lcd.setCursor(0,0);
+  lcd.print("SET ME UP");
+}
+byte getCounter(byte num){
+  currentStateCLK = digitalRead(CLK);
+   if (currentStateCLK != lastStateCLK&&currentStateCLK==1){
     if (digitalRead(DT) != currentStateCLK) {
-      counter --;
-      Serial.println(counter);
+      num --;
+      Serial.println(num);
     } else {
-      counter ++;
-      Serial.println(counter);
+      num ++;
+      Serial.println(num);
     }
-    if(setupState==SETHOUR){
-        Serial.println(setupState);
-        tm.Hour+=counter;
-        t = makeTime(tm);
-        RTC.set(t);        
-        setTime(t);
-        printDateTime(t);
-      }else if(setupState==SETMIN){
-        Serial.println(setupState);
-        tm.Minute+=counter;
-        t = makeTime(tm);
-        RTC.set(t);        
-        setTime(t);
-        printDateTime(t);
-      }else if(setupState==SETSEC){
-        Serial.println(setupState);
-        tm.Second+=counter;
-        t = makeTime(tm);
-        RTC.set(t);        
-        setTime(t);
-        printDateTime(t);
-      }
    }
    lastStateCLK=currentStateCLK;
+   return num;
 }
-// print date and time to Serial
-void printDateTime(time_t t){
-    printDate(t);
-    printTime(t);
-}
-// print time to Serial
-void printTime(time_t t){
-    lcd.setCursor(0,1);
-    printI00(hour(t), ':');
-    lcd.setCursor(3,1);
-    printI00(minute(t), ':');
-    lcd.setCursor(6,1);
-    printI00(second(t), ' ');
-}
-// print date to Serial
-void printDate(time_t t){
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(setupDone){
+    
+    printToLCD();
+  }else{
+    checkSwitchCondition();
+   if(setupState==SETDAY){
+    lcd.clear();
+    byte currDay=RTC.getDate();
+    byte setupDate=getCounter(currDay);
+    RTC.setDate(setupDate);
     lcd.setCursor(0,0);
-    printI00(day(t), 0);
-    lcd.print(monthShortStr(month(t)));
-    lcd.print(year(t));
-    lcd.print(' ');
-   // Serial << monthShortStr(month(t)) << _DEC(year(t));
-}
-// Print an integer in "00" format (with leading zero),
-// followed by a delimiter character to Serial.
-// Input value assumed to be between 0 and 99.
-void printI00(int val, char delim){
-    if (val < 10) lcd.print("0");
-    lcd.print(val);
-    if (delim > 0) lcd.print(delim);
-    return;
-}
-void printDateTimeTemperature(time_t t,float c,float f){
-    printDateTime(t);
-    lcd.setCursor(10,1);
-    lcd.print(c);
-    lcd.print("C");
+    lcd.print(RTC.getDate(),DEC);
+    lcd.setCursor(2,0);
+    lcd.print("/");
+   }else if(setupState==SETMON){
+    byte currMonth=RTC.getMonth(century);
+    byte setupMonth=getCounter(currMonth);
+    RTC.setMonth(setupMonth);
+    lcd.setCursor(3,0);
+    lcd.print(RTC.getMonth(century),DEC);
+    lcd.setCursor(5,0);
+    lcd.print("/");
+   }else if(setupState==SETYEAR){
+    byte currYear=RTC.getYear();
+    byte setupYear=getCounter(currYear);
+    RTC.setYear(setupYear);
+    lcd.setCursor(6,0);
+    lcd.print("20");
+    lcd.setCursor(8,0);
+    lcd.print(RTC.getYear(),DEC);
+    lcd.setCursor(11,0);
+    lcd.print(DoW[RTC.getDoW()-1]);
+   }else if(setupState==SETDOW){
+    byte currDoW=RTC.getDoW();
+    byte setupDoW=getCounter(currDoW);
+    RTC.setDoW(setupDoW);
+    lcd.setCursor(6,0);
+    lcd.print("20");
+    lcd.setCursor(8,0);
+    lcd.print(RTC.getYear(),DEC);
+    lcd.setCursor(11,0);
+    lcd.print(DoW[RTC.getDoW()-1]);
+   }else if(setupState==SETHOUR){
+    byte currHour=RTC.getHour(h12Flag,pmFlag);
+    byte setupHour=getCounter(currHour);
+    RTC.setHour(setupHour);
+    lcd.setCursor(0,1);
+    lcd.print(RTC.getHour(h12Flag,pmFlag),DEC);
+    lcd.setCursor(2,1);
+    lcd.print(":");
+   }else if(setupState==SETMIN){
+    byte currMinutes=RTC.getMinute();
+    byte setupMinutes=getCounter(currMinutes);
+    RTC.setMinute(setupMinutes);
+    lcd.setCursor(3,1);
+    lcd.print(RTC.getMinute(),DEC);
+    lcd.setCursor(5,1);
+    lcd.print(":");
+   }else if(setupState==SETSEC){
+    byte currSecond=RTC.getSecond();
+    byte setupSecond=getCounter(currSecond);
+    RTC.setSecond(setupSecond);
+    lcd.setCursor(6,1);
+    lcd.print(RTC.getSecond(),DEC);
+    help=true;
+   }else if(help){
+    setupDone=true;
+   }
+  }
 }
