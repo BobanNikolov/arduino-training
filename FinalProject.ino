cef49ab9314fb57d92c00259a13397318fadf0db
Boban <56995138+BobanNikolov@users.noreply.github.com>
2021-02-25 11:58:12 +0100

    Refactored code and added comments

--- FinalProject.ino
+++ FinalProject.ino
@@ -1,17 +1,12 @@
+//including needing libraries
 #include <SdFat.h>
-//#include "RTClib.h"
 #include <SimpleDHT.h>
 #include <SPI.h>
-//#include <SD.h>
 #include <Wire.h>
 #include <DS3231.h>
 #include <LiquidCrystal_I2C.h>
 #include "PushButton.h"
-DS3231 RTC;
-RTClib rtc;
-LiquidCrystal_I2C lcd(0x27,16,2);
-#define RTC_TYPE 2
-#define SD_FAT_TYPE 0
+//defining needing variables (used so I don't forget which pin is for what)
 #define echoPin 7
 #define trigPin 6
 #define buz 9
@@ -23,31 +18,36 @@ LiquidCrystal_I2C lcd(0x27,16,2);
 #define buttonNOCHANGE 3
 #define DHTTYPE DHT11
 #define DHT_PIN 8
+//initializing objects of given classes
+DS3231 RTC;
+RTClib rtc;
+LiquidCrystal_I2C lcd(0x27,16,2);
 SimpleDHT11 dht11(DHT_PIN);
 SdFat sd;
 File myFile;
-char fileName[20]="\0";
+PushButton buttonSETUP(SW);
+char fileName[20]="\0"; //variable for keeping the name of the file
 long duration; // variable for the duration of sound wave travel
 int distance;  // variable for the distance measurement
-boolean setupDone=false;
-enum clockState { UNDEFINED=0, RUNNING, SETDAY, SETMON, SETYEAR, SETDOW, SETHOUR, SETMIN, SETSEC } setupState;
-PushButton buttonSETUP(SW);
-byte temperature = 0;
-byte humidity = 0;
-int finalHumidity;
-int finalTemperature;
-int err = SimpleDHTErrSuccess;
-bool help=false;
-bool century = false;
-bool h12Flag=false;
-bool pmFlag=false;
-String DoW[7]={"MON","TUE","WED","THU","FRI","SAT","SUN"};
-char timeString[50];
-int counter=0;
+boolean setupDone=false; //flag for noticing when the setup is done
+enum clockState { UNDEFINED=0, RUNNING, SETDAY, SETMON, SETYEAR, SETDOW, SETHOUR, SETMIN, SETSEC } setupState;//enum for state of setup button
+byte temperature = 0; //measuring temperature
+byte humidity = 0; //measuring humidity
+int finalHumidity; //setting up humidity for printing
+int finalTemperature; //setting up temperature for printing
+int err = SimpleDHTErrSuccess; //checking if DHT sensor has any problems
+bool help=false; //flag for checking when to print
+bool century = false; //flag for .getMonth()
+bool h12Flag=false; //flag for .getHour()
+bool pmFlag=false; //flag for .getHour()
+String DoW[7]={"MON","TUE","WED","THU","FRI","SAT","SUN"}; //string array for days of week
+char timeString[50]; //char array for printing the current time
+int counter=0; //counter for adding or subtracting while in setupmode
 int timer;
 int currentStateCLK;
 int lastStateCLK;
 byte buttonStateSETUP=buttonNOCHANGE;
+//function for getting the current date and time and setting it up as the date and time on file that goes to SdCard
 void dateTime(uint16_t* date, uint16_t* time, uint8_t* ms10) {
   DateTime now = rtc.now();
 
@@ -60,6 +60,7 @@ void dateTime(uint16_t* date, uint16_t* time, uint8_t* ms10) {
   // Return low time bits in units of 10 ms, 0 <= ms10 <= 199.
   *ms10 = now.second() & 1 ? 100 : 0;
 }
+//function for checking switch condition of setupState and settuping the state that we are in
 void checkSwitchCondition(void){
   buttonStateSETUP=buttonSETUP.stateChanged();
   if(buttonStateSETUP==buttonUP){
@@ -120,6 +121,7 @@ void checkSwitchCondition(void){
       }
   }
 }
+//function to printing to LCD while checking is it correct time and date so it doesn't overlap
 void printToLCD(){
   byte timeToCheck;
   if((int)RTC.getDate()<10){
@@ -199,24 +201,7 @@ void printToLCD(){
   lcd.setCursor(15,1);
   lcd.print((char)223);
 }
-void setup() {
-  // put your setup code here, to run once:
-  lcd.init();
-  lcd.backlight();
-  Wire.begin();
-  Serial.begin(9600);
-  pinMode(DHT_PIN, INPUT_PULLUP);
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
-  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
-  pinMode(CLK,INPUT);
-  pinMode(DT,INPUT);
-  pinMode(SW, INPUT_PULLUP);
-  sd.begin(10);
-  lastStateCLK = digitalRead(CLK);
-  setupState=RUNNING;
-  lcd.setCursor(0,0);
-  lcd.print("SET ME UP");
-}
+//function to correct date
 byte correctDate(byte num){
   if (setupState==SETDAY){
     if(num<0){
@@ -245,6 +230,7 @@ byte correctDate(byte num){
   }
   return num;
 }
+//function for adding or subtracting
 byte getCounter(byte num){
   currentStateCLK = digitalRead(CLK);
    if (currentStateCLK != lastStateCLK&&currentStateCLK==1){
@@ -260,6 +246,24 @@ byte getCounter(byte num){
    }
    lastStateCLK=currentStateCLK;
    return num;
+}
+void setup() {
+  // put your setup code here, to run once:
+  lcd.init();
+  lcd.backlight();
+  Wire.begin();
+  Serial.begin(9600);
+  pinMode(DHT_PIN, INPUT_PULLUP);// Sets DHT sensor
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  pinMode(CLK,INPUT); //Sets rotary encoder
+  pinMode(DT,INPUT);//Sets rotary encoder
+  pinMode(SW, INPUT_PULLUP);//Sets rotary encoder
+  sd.begin(10);//Sets SdCard
+  lastStateCLK = digitalRead(CLK); //Sets rotary encoder
+  setupState=RUNNING; //first state
+  lcd.setCursor(0,0);
+  lcd.print("SET ME UP");
 }
 void loop() {
   // put your main code here, to run repeatedly:
